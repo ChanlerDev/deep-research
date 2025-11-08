94d34b414477c84d5e783962b6f4e74fd8d2e597
Merryfling <merryfling@gmail.com>
2025-11-09 01:11:42 +0800

    chore: initialize deep-research project

---
+++ src/main/java/dev/chanler/researcher/DeepResearchApplication.java
@@ -0,0 +1,13 @@
+package dev.chanler.researcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/chanler/researcher/DeepResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chanler.researcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
